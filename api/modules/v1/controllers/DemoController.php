b818bf14a3c10fc1c5bba368e8d28196008134b4
DESKTOP-PPATCSO\baiyou <dengcy@caiyoudata.com>
2018-12-10 18:02:21 +0800

    测试git hook

--- api/modules/v1/controllers/DemoController.php
+++ api/modules/v1/controllers/DemoController.php
@@ -29,7 +29,7 @@ class DemoController extends \yii\rest\ActiveController
 
     public function actionIndex()
     {
-        return ['message'=>'api9哇哇问问11通'];
+        return ['message'=>'api9哇哇问热热热通'];
 
     }
 }
