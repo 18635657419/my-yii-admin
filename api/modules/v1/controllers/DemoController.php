73fb7a51de2809b6db808e039ef3159d4d66837f
DESKTOP-PPATCSO\baiyou <dengcy@caiyoudata.com>
2018-12-10 15:04:02 +0800

    cs

--- api/modules/v1/controllers/DemoController.php
+++ api/modules/v1/controllers/DemoController.php
@@ -29,7 +29,7 @@ class DemoController extends \yii\rest\ActiveController
 
     public function actionIndex()
     {
-        return ['message'=>'api通通'];
+        return ['message'=>'api999通'];
 
     }
 }
